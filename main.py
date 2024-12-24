424d65d332b8d71649a1917c023930063206cb1a
Bryce Walsh <bwalsh@mines.edu>
2024-12-24 15:39:48 -0500

    Initial commit and basic selenium setup

---
+++ main.py
@@ -0,0 +1,10 @@
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+
+driver = webdriver.Chrome(ChromeDriverManager().install())
+
+driver.get("https://www.safeway.com/")
+
+title = driver.title
+
+print(title)
