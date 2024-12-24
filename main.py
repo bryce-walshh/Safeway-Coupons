369d3bbfb18be7831b364e1a57c8b7eb7f683fa5
Bryce Walsh <bwalsh@mines.edu>
2024-12-24 18:26:08 -0500

    Read login info from file

--- main.py
+++ main.py
@@ -3,6 +3,13 @@ from selenium.webdriver.common.by import By
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.keys import Keys
 import time
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+with open("logininfo.txt", "r") as file:
+    lines = file.readlines()
+    phone_num = lines[0].strip() 
+    password = lines[1].strip()
 
 driver = webdriver.Chrome(ChromeDriverManager().install())
 
@@ -12,11 +19,25 @@ driver.get("https://www.safeway.com/foru/coupons-deals.html")
 
 login = driver.find_element(By.ID, 'enterUsername')
 
-phone_num = input("Phone Number: ")
-password = input("Password: ")
-
 login.send_keys(phone_num)
 login.send_keys(Keys.RETURN)
+
+try:
+    password_inp = WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located((By.ID, 'password'))
+    )
+finally:
+    print(driver.title)
+    #driver.quit()
+
+
+password_inp = driver.find_element(By.ID, 'password')
+password_inp.send_keys(password)
+
+time.sleep(5)
+
+password_inp.send_key(Keys.RETURN)
+
 
 time.sleep(5)
 
