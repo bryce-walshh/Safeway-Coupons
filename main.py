8a1a4504e1ac54c64e2d51dce9285b53da611966
Bryce Walsh <bwalsh@mines.edu>
2024-12-24 17:54:05 -0500

    Got login correctly

--- main.py
+++ main.py
@@ -1,10 +1,23 @@
 from selenium import webdriver
+from selenium.webdriver.common.by import By
 from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.keys import Keys
+import time
 
 driver = webdriver.Chrome(ChromeDriverManager().install())
 
-driver.get("https://www.safeway.com/")
+driver.get("https://www.safeway.com/foru/coupons-deals.html")
 
-title = driver.title
+# inputting into login
 
-print(title)
+login = driver.find_element(By.ID, 'enterUsername')
+
+phone_num = input("Phone Number: ")
+password = input("Password: ")
+
+login.send_keys(phone_num)
+login.send_keys(Keys.RETURN)
+
+time.sleep(5)
+
+driver.quit()
